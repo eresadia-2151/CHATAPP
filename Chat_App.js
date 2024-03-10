4d5b5c2c0f27bf8bec7243a7e3e137f012e4b7f1
Eresedia Chademuwiri <chademuwirie@icloud.com>
2024-03-10 23:55:28 +0200

    commit 4

--- Chat_App.js
+++ Chat_App.js
@@ -6,6 +6,8 @@ const chatInputForm = document.querySelector('.chat-input-form')
 const chatInput = document.querySelector('.chat-input')
 const clearChatBtn = document.querySelector('.clear-chat-button')
 
+const messages = JSON.parse(localStorage,getItem('messages')) || []
+
 const createChatMessageElement = (message) => `
 <div class="message ${message.sender === 'Lecturer' ? 'blue-bg' : 'gray-bg'}">
  <div class="message-sender">${message.sender}</div>
@@ -13,6 +15,12 @@ const createChatMessageElement = (message) => `
  <div class="message-timestamp">${message.timestamp}</div>
 </div>
 `
+
+window.onload = () =>{
+    messages.forEach((message) => {
+        chatMessages.innerHTML += createChatMessageElement(message)
+    })
+}
 
 let messageSender = 'Lecturer'
 
@@ -46,6 +54,8 @@ const sendMessage = (e) => {
         timestamp,
     }
 
+    messages.push(message)
+    localStorage.setItem('messages', JSON.stringify(message))
     chatMessages.innerHTML += createChatMessageElement(message)
 
     chatInputForm.reset()
@@ -53,3 +63,8 @@ const sendMessage = (e) => {
 }
 
 chatInputForm.addEventListener('submit', sendMessage)
+
+clearChatBtn.addEventListener('click', () => {
+    localStorage.clear()
+    chatMessages.innerHTML = ''
+})
